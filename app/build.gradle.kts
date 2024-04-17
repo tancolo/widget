b9c726451f7d40281bff06af34eb1848408ca2d5
john.tan <shrimpcolo@gmail.com>
2024-04-17 14:48:49 +0800

    Add an App demo to test CountdownCircleView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -41,6 +43,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(project(":customview"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/tancolo/widget/MainActivity.kt
+++ app/src/main/java/com/tancolo/widget/MainActivity.kt
@@ -2,10 +2,59 @@ package com.tancolo.widget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.tancolo.customview.countdownview.CountdownCircleView
 
 class MainActivity : AppCompatActivity() {
+
+    private val mCountdownCircleViewList = ArrayList<CountdownCircleView>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
     }
+
+    private fun initView() {
+        findViewById<Button>(R.id.button_show).setOnClickListener {
+            // start all the countdown circle view.
+            mCountdownCircleViewList.map {
+                it.start()
+            }
+        }
+
+        // Add all the countdown view to ArrayList
+        // linearlayout 1
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_backward))
+        mCountdownCircleViewList.add(findViewById(R.id.view_counterclockwise_forward))
+        mCountdownCircleViewList.add(findViewById(R.id.view_counterclockwise_backward))
+
+        // linearlayout 2
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward_2_001))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward_2_002))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward_2_003))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward_2_004))
+
+        // linearlayout 3
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_backward_2_001))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_backward_2_002))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_backward_2_003))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_backward_2_004))
+
+        // linearlayout 4
+        mCountdownCircleViewList.add(findViewById(R.id.view_counterclockwise_forward_4_001))
+        mCountdownCircleViewList.add(findViewById(R.id.view_counterclockwise_forward_4_002))
+        mCountdownCircleViewList.add(findViewById(R.id.view_counterclockwise_forward_4_003))
+        mCountdownCircleViewList.add(findViewById(R.id.view_counterclockwise_forward_4_004))
+
+        // linearlayout 5
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward_5_001))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward_5_002))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward_5_003))
+        mCountdownCircleViewList.add(findViewById(R.id.view_clockwise_forward_5_004))
+
+    }
+
 }
